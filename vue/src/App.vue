87433eb0340b489b4683aa86e9d600d2b1b6fa3c
Tyler Sparrow <t.sparrow55@gmail.com>
2023-12-05 13:50:19 -0500

    added nav bar to app.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="capstone-app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link> |
-      <router-link v-bind:to="{name: 'newExample'}" v-show="$store.state.token != ''">Add New Snippet</router-link>
+  <div class="main">
+    <aside class="topnav">
+      <li><img src="src\assets\techelevatorimage.png" alt="Logo" class="logo" v-on:click="returnHome"></li>
+
+      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
+      <div id="yes">
+
+      <li><router-link id="tab" v-bind:to="{ name: 'newExample' }" v-show="$store.state.token != ''">Add New Snippet</router-link> </li>
+      <li><router-link id="tab" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link></li>
+
     </div>
-    <router-view />
+    </aside>
+    <router-view class="body"/>
   </div>
 </template>
 
@@ -13,8 +19,60 @@
 export default {
   created() {
     this.$store.commit("LOAD_EXAMPLES");
-  }
+  },
+  methods: {
+    returnHome() {
+      this.$router.push({ name: 'home' })
+    }
+  },
 }
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.body{
+  padding: 16px;
+  margin-top: 30px;
+  height: 1500px;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #f5f5f5;;
+  position: fixed;
+  list-style-type: none;
+  top: 0;
+  width: 100%;
+  white-space:nowrap;
+  display:inline-flex;
+
+}
+#yes{
+  position:fixed;
+  padding-left: 70%;
+}
+.topnav li {
+
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  display:inline-flex;
+
+}
+#tab{
+color:#00ADEE;
+text-decoration: none;
+}
+.logo {
+  width: 60px;
+  height: auto;
+  margin-right: 1rem;
+  cursor: pointer;
+
+}
+</style>
 
 
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
     <router-link v-bind:to="{ name: 'listExamples' }">Show Examples</router-link>
   </div>
 </template>
